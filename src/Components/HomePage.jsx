eae820654e3b585b2691f13b9f1a2707be0420dc
Lovish7905 <91596563+Lovish7905@users.noreply.github.com>
2024-08-01 20:18:53 +0530

    Update HomePage.jsx

--- src/Components/HomePage.jsx
+++ src/Components/HomePage.jsx
@@ -102,7 +102,7 @@ function HomePage() {
           </div>
         </div>
         {/*Right Side Task Showing Panel*/}
-        <div className="flex border-solid border-gray-950  w-full bg-slate-700  justify-evenly xxs:flex-wrap sm:flex-wrap md:flex-wrap lg:flex-nowrap  ">
+        <div className="flex border-solid border-gray-950  w-full bg-slate-700  justify-evenly  sm:flex-wrap md:flex-wrap lg:flex-nowrap  ">
           <div
             className="border border-solid w-full  "
             droppable
